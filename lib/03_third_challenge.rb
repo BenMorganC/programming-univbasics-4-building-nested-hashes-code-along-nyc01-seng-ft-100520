c4f9cd02523bf246fed408a87d5114b683ebd5a9
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-17 22:37:18 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -40,8 +40,7 @@ def third_challenge
       :heroine_friends => [
         {:name => "Steven",
         :age => "30",
-        :attitude => "confused"}
-      ,
+        :attitude => "confused"},
         {:name => "Nurse",
         :age => "44",
         :attitude => "worried"}
