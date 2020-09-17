403b68b8c9192034c0bf94f63f5021ea3775cba8
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-17 22:22:58 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -14,7 +14,11 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :hero_friends => []
+      :hero_friends => [{
+        :name => "Benvolio",
+        :age => "",
+        :attitude => ""
+      }]
    }, 
    :capulet => {
       :patriarch => {
